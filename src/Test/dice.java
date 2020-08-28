808eacf99506454aa28abcb278f6cc90aa061ee1
MrThomminator <t.locher@gmx.de>
2020-08-28 14:57:37 +0200

    schwarzwuscht

--- src/Test/dice.java
+++ src/Test/dice.java
@@ -5,6 +5,7 @@ public class dice {
 	public static void main(String[] args) {
 		String test = "Thomas isch doof.";
 		System.out.println(test);
+		System.out.println("sagsch du, du schwarzwuscht");
 
 	}
 
