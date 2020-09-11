edcac048dc9f50fc75fcd528c65952c9b6b35dc5
tulpafred <qmbeLxrcF2tQDcVTMWtr>
2020-09-11 08:58:43 +0200

    ...

---
+++ src/GameCollection/Genre.java
@@ -0,0 +1,30 @@
+package GameCollection;
+
+public enum Genre {
+
+	/*
+	 * enum Auswahl
+	 */
+	
+	ACTION("Action"), ACTION_ADVENTURE("Action Adventure"), RPG("RPG"), SPORTS("Sport"), STRATEGY ("Strategie"), SIMULATION ("Simulation");
+	
+	/*
+	 * Attribute
+	 */
+	
+	private String name;
+	
+	/*
+	 * Methoden
+	 */
+	
+	Genre (String name) {
+		this.name = name;
+	}
+	
+	public String getName () {
+		return name;
+	}
+	
+	
+}
