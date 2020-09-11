33eb1e8b5f6bf8550225610766bc6b0dd7b25616
MrThomminator <t.locher@gmx.de>
2020-09-11 09:08:26 +0200

    ...

--- src/GameCollection/Main.java
+++ src/GameCollection/Main.java
@@ -1,10 +1,40 @@
 package GameCollection;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Main extends Application{
 
+	public static void main(String[] args){
+		launch(args);
+
+	}
+
+	@Override
+	public void init() throws Exception {
+		
+//		super.init();
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		new FXMLLoader();
+		Parent root = FXMLLoader.load(getClass().getResource("myGamesView.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("Melissas, Julians und Thomas Spieleliste");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		//primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("laubbaumicon.jpg")));
+	
+	}
+	
+	@Override
+	public void stop() {
+		
 	}
 
 }
