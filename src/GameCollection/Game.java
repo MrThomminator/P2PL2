c04a4ac125ca9c40380479663c0d783a99355c71
tulpafred <qmbeLxrcF2tQDcVTMWtr>
2020-09-11 09:09:26 +0200

    ...

--- src/GameCollection/Game.java
+++ src/GameCollection/Game.java
@@ -21,5 +21,47 @@ public class Game implements Serializable {
 	public Game (String title, Genre genre, int releasedate, boolean isPlayed) {
 		this.title = title;
 		this.genre = genre;
+		this.releasedate = releasedate;
+		this.isPlayed = isPlayed;
 	}
+
+	/*
+	 * Getter und Setter
+	 */
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+
+	public int getReleasedate() {
+		return releasedate;
+	}
+
+	public void setReleasedate(int releasedate) {
+		this.releasedate = releasedate;
+	}
+
+	public boolean isPlayed() {
+		return isPlayed;
+	}
+
+	public void setPlayed(boolean isPlayed) {
+		this.isPlayed = isPlayed;
+	}
+	
+	
+	
+	
 }
