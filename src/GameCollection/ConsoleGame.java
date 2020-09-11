448e2ae6a7c85b4679ec3cbca5309375fa8a39fc
MrThomminator <t.locher@gmx.de>
2020-09-11 10:07:25 +0200

    ...

---
+++ src/GameCollection/ConsoleGame.java
@@ -0,0 +1,33 @@
+package GameCollection;
+
+import java.io.Serializable;
+public class ConsoleGame extends Game implements Serializable {
+
+	/*
+	 * Attribute
+	 */
+	
+<<<<<<< HEAD
+=======
+	private static final long serialVersionUID = 1L;
+>>>>>>> branch 'master' of https://github.com/MrThomminator/P2PL2
+	private ConsoleSystem consoleSystem;
+	
+	/*
+	 * Methoden
+	 */
+	
+	public ConsoleGame(String title, Genre genre, int releasedate, boolean isPlayed, ConsoleSystem consoleSystem) {
+		super(title, genre, releasedate, isPlayed);
+		this.consoleSystem = consoleSystem;
+	}
+
+	public ConsoleSystem getConsoleSystem() {
+		return consoleSystem;
+	}
+
+	public void setConsoleSystem(ConsoleSystem consoleSystem) {
+		this.consoleSystem = consoleSystem;
+	}
+
+}
